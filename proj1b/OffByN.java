24698c8677f633edc454bad8968fbf335d7e7f6d
xoxoboyu <xoxo.boyu@gmail.com>
2021-05-10 20:30:08 +0800

    revise OffBy_

--- proj1b/OffByN.java
+++ proj1b/OffByN.java
@@ -12,6 +12,8 @@ public class OffByN implements CharacterComparator {
 
     @Override
     public boolean equalChars(char x, char y) {
+        x = Character.toLowerCase(x);
+        y = Character.toLowerCase(y);
         return (x - y == n || y - x == n);
     }
 }
--- proj1b/OffByOne.java
+++ proj1b/OffByOne.java
@@ -5,10 +5,8 @@
 public class OffByOne implements CharacterComparator {
     @Override
     public boolean equalChars(char x, char y) {
-//        if ( x - y == 1 || y - x == 1) {
-//            return true;
-//        }
-//        return false;
+        x = Character.toLowerCase(x);
+        y = Character.toLowerCase(y);
         return ( x - y == 1 || y - x == 1);
     }
 }
